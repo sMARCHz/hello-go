d1480220e7753ab5deba8c5bf169bc480df6c1ee
Nattanon Chansamakr <mart_nattanon@hotmail.com>
2022-05-02 21:40:52 +0700

    Update comments in array section

--- arrays/main.go
+++ arrays/main.go
@@ -12,20 +12,20 @@ func main() {
 	arr[2] = "three"
 	fmt.Println(arr)
 
-	// compact declaration
+	// Compact initialization
 	arr2 := [3]string{"one", "two", "three"}
 	fmt.Println(arr2)
 
-	// Array is a fixed-length sequence so, each index allocates the same amount of memory (i.e. each index of int array allocates 8 bytes)
+	// Array is a fixed-length sequence so, each elements allocate the same amount of memory (i.e. array of type int allocates 8 bytes per element)
 	// !! Unable to do pointer arithmetic
 	arr3 := [2]int{1, 2}
 	fmt.Println(arr3, &arr3[0], &arr3[1])
 
-	// declare with dynamic length
+	// Initialize with dynamic length
 	arr4 := [...]bool{true, true, false, true}
 	fmt.Println(arr4)
 
-	// Print each index's value
+	// Access each elements via iteration
 	// For-each
 	for k, v := range arr2 {
 		fmt.Printf("Index[%v] = %v\n", k, v)
