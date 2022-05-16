701029c5aaa7dd7361c0c9c10ff21c235a090cbd
Nattanon Chansamakr <mart_nattanon@hotmail.com>
2022-05-16 19:56:37 +0700

    Update comment in struct section

--- structs/main.go
+++ structs/main.go
@@ -71,7 +71,7 @@ func main() {
 
 	// A tag for a field allows you to attach meta-information to the field which can be acquired using reflection.
 	// Usually it is used to provide transformation info on how a struct field is encoded to or decoded from another format (or stored/retrieved from a database), but you can use it to store whatever meta-info you want to, either intended for another package or for your own use.
-	// Example -> `json:"name"`
+	// Example  `json:"name"` -> When we encode it to json, the field's name will be 'name' instead of origin name
 	type Animal2 struct {
 		name   string `required max:"100"`
 		origin string
